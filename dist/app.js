1850159a67c2039cf30fda3f9f6a2e442aae08e4
Almerindo Paixão <almerindopaixao@gmail.com>
2020-07-08 08:15:13 -0300

    atualização

--- dist/app.js
+++ dist/app.js
@@ -6,6 +6,8 @@ _dotenv2.default.config();
 require('./database/index'); // importando o database
 // Importando o express
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 
 // Rotas
 var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interopRequireDefault(_tokenRoutes);
@@ -13,6 +15,21 @@ var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopReq
 var _usuarioRoutes = require('./routes/usuarioRoutes'); var _usuarioRoutes2 = _interopRequireDefault(_usuarioRoutes);
 var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interopRequireDefault(_alunoRoutes);
 var _fotoRoutes = require('./routes/fotoRoutes'); var _fotoRoutes2 = _interopRequireDefault(_fotoRoutes);
+
+const whiteList = [
+  'https://react2.otaviomiranda.com.br',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   /**
@@ -25,6 +42,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(_cors2.default.call(void 0, corsOptions));
+    this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use(_express2.default.static(_path.resolve.call(void 0, __dirname, 'uploads')));
--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ dotenv.config();
 import './database/index'; // importando o database
 // Importando o express
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 // Rotas
 import tokenRoutes from './routes/tokenRoutes';
@@ -13,6 +15,21 @@ import homeRoutes from './routes/homeRoutes';
 import usuarioRoutes from './routes/usuarioRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import fotoRoutes from './routes/fotoRoutes';
+
+const whiteList = [
+  'https://react2.otaviomiranda.com.br',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 
 class App {
   /**
@@ -25,6 +42,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
